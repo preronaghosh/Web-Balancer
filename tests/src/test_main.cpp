2af51a27d5fa42ee1f7b3ca1803de917a769c788
Prerona <preronaghosh1997@gmail.com>
2024-06-09 13:14:02 -0400

    fixing the test

--- tests/src/test_main.cpp
+++ tests/src/test_main.cpp
@@ -2,10 +2,10 @@
 
 TEST(MyTestSuite, MyFirstTest) {
     // Your test code goes here
-    ASSERT_EQ(2 + 2, 5);  // Example assertion
+    ASSERT_EQ(2 + 2, 4);  // Example assertion
 }
 
 TEST(MyTestSuite, MySecondTest) {
     // Your test code goes here
-    ASSERT_EQ(2 + 1, 4);  // Example assertion
+    ASSERT_EQ(2 + 1, 3);  // Example assertion
 }
